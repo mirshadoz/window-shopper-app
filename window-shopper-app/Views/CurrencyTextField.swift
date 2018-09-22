2c598f1d3dc51e6c1833bafe9954423126fe5dcf
Mirshad Oz <mirshadoz@mirshads-macbook-pro.local>
2018-09-22 15:00:13 +0600

    Implement CustomTextField, InputAccessory, IBDesignable

---
+++ window-shopper-app/Views/CurrencyTextField.swift
@@ -0,0 +1,35 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper-app
+//
+//  Created by Mirshad Oz on 9/22/18.
+//  Copyright © 2018 Mirshad Oz. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CurrencyTextField: UITextField {
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView() {
+        backgroundColor = #colorLiteral(red: 0.9961728454, green: 0.9902502894, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let tempPlace = placeholder {
+            let otherPlace = NSAttributedString(string: tempPlace, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = otherPlace
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
+}
