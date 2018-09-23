841defd44c872d0b956157569cc84a74fa4bca26
Mirshad Oz <mirshadoz@mirshads-macbook-pro.local>
2018-09-23 12:37:26 +0600

    Implement custom view with currency symbol

--- window-shopper-app/Views/CurrencyTextField.swift
+++ window-shopper-app/Views/CurrencyTextField.swift
@@ -11,6 +11,25 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.7353103757, green: 0.7309411168, blue: 0.7386702895, alpha: 0.8)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        // Location thing with Currency
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        
+        addSubview(currencyLbl)
+        
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -24,6 +43,8 @@ class CurrencyTextField: UITextField {
         backgroundColor = #colorLiteral(red: 0.9961728454, green: 0.9902502894, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        
+        clipsToBounds = true
         
         if let tempPlace = placeholder {
             let otherPlace = NSAttributedString(string: tempPlace, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
